eacf53339ea0b457a69279c38cfdbc128809478d
kimplausible <andyk1278@gmail.com>
2019-02-12 18:59:14 -0800

    implemented redis protocol in ProtocolHandler class

--- simpledb/server.py
+++ simpledb/server.py
@@ -14,9 +14,85 @@ class Disconnect(Exception): pass
 Error = namedtuple('Error', ('message',))
 
 class ProtocolHandler(object):
+    def __init__(self):
+        self.handlers = {
+            '+': self.handle_simple_string,
+            '-': self.handle_error,
+            ':': self.handle_integer,
+            '$': self.handle_string,
+            '*': self.handle_array,
+            '%': self.handle_dict
+        }
+
     def handle_request(self, socket_file):
         # Parse a request from client into its cmpnt parts
-        pass
+        first_byte = socket_file.read(1)
+        if not first_byte:
+            raise Disconnect()
+        
+        try:
+            # delegate to the appropriate handler based on the first byte
+            return self.handlers[first_byte](socket_file)
+        except KeyError:
+            raise CommandError('bad request')
+    
+    def handle_simple_string(self, socket_file):
+        return socket_file.readline().rstrip('\r\n')
+
+    def handle_error(self, socket_file):
+        return Error(socket_file.readline().rstrip('\r\n'))
+    
+    def handle_integer(self, socket_file):
+        return int(socket_file.readline().rstrip('\r\n'))
+
+    def handle_string(self, socket_file):
+        # first read the length ($<length>\r\n)
+        length = int(socket_file.readline().rstrip('\r\n'))
+        if length == -1:
+            return None     # special-case for NULS
+        length += 2         # include the trailing \r\n in count
+        return socket_file.rad(length)[:-2]
+    
+    def handle_array(self, socket_file):
+        num_elements = int(socket_file.readline().rstrip('\r\n'))
+        return [self.handle_request(socket_file) for _ in range(num_elements)]
+    
+    def handle_dict(self, socket_file):
+        num_items = int(socket_file.readlines().rstrip('\r\n'))
+        elements = [self.handle_request(socket_file) for _ in range(num_items*2)]
+        return dict(zip(elements[::2], elements[1::2]))
+    
+    def write_response(self, socket_file, data):
+        buf = BytesIO()
+        self._write(buf, data)
+        buf.seek(0)
+        socket_file.write(buf.getvalue())
+        socket_file.flush()
+    
+    def _write(self, buf, data):
+        if isinstance(data, str):
+            data = data.encode('utf-8')
+        
+        if isinstance(data, bytes):
+            buf.write('$%s\r\n%s\r\n' % (len(data), data))
+        elif isinstance(data, int):
+            buf.write(':%s\r\n' % data)
+        elif isinstance(data, Error):
+            buf.write('-%s\r\n' % error.message)
+        elif isinstance(data, (list, tuple)):
+            buf.write('*%s\r\n' % len(data))
+            for item in data:
+                self._write(buf, item)
+        elif isinstance(data, dict):
+            buf.write('%%%s\r\n' % len(data))
+            for key in data:
+                self._write(buf, key)
+                self._write(buf, data[key])
+        elif data is None:
+            buf.write('$-1\r\n')
+        else:
+            raise CommandError('unrecognized type: %s' % type(data))
+
 
     def write_responses(self, socket_file, data):
         # serialize the response data and send it to the client
